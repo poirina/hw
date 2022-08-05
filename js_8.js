7d8c7d30531de13a2f2bdb08fa1ce2067c72bdcd
poirina <90609904+poirina@users.noreply.github.com>
2022-08-05 12:59:52 +0300

    правильный

--- js_8.js
+++ js_8.js
@@ -1,3 +1,6 @@
-let a=8;
-let b=3;
-console.log(x);
+let z=8;
+let v=3;
+let x;
+console.log("Задание 8 уравнение 1",x=(16-z)/2+v)
+console.log("Задание 8 уравнение 2",x=(z-15*v)/(v-6))
+console.log("Задание 8 уравнение 3",x=23780/(1+2+z+v))
