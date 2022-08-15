4db6b5d0cd51336f0d8356564f383f8d3d6d58ae
poirina <90609904+poirina@users.noreply.github.com>
2022-08-15 21:50:06 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -28,10 +28,8 @@ while (i<=56){
 //task 4
 let arr4=["10", "20", "30", "50", "235", "3000"];
 for(i=0; i<arr4.length; i++){
-    str=arr4[i]+" ";
-    console.log(str);
-    if(str[0] === '1' || str[0] === '2' || str[0] === '5'){
-        console.log(arr4);
+    if(arr4[i][0] == 1 || arr4[i][0] == 2 || arr4[i][0] == 5){
+        console.log(arr4[i]);
     }
 }
 //task5
@@ -65,10 +63,16 @@ for(i = 0; ; i++){
 console.log(arr7);
 //task8
 let arr8 = [12, false, 'Текст', 4, 2, -5, 0];
+let arrBuffer = [];
 while(i <= arr8.length){
     arr8.reverse();
     i++;
 }
+//while (arr8.length){ работает пока в масиве есть хотя бы один эллемент
+//  let but = arr8.pop();
+//but =but.toString().split('').reverse().join('')
+//arrBuffer.push(but)
+//}
 console.log(arr8);
 //task9 
 let arr9 = [5, 9, 21,' ',' ' , 9, 78,' ' ,' ' ,' ' , 6];
@@ -79,3 +83,44 @@ for(i = 0; i < arr9.length; i++){
     }
 }
 console.log(count);
+//task 10
+let arr10_1 =[48,9,0,4,21,2,1,0,8,84,76,8,4,13,2];
+let arr10_2 = [1,8,0,13,76,8,7,0,22,0,2,3,2];
+let indexArr10_1 = arr10_1.indexOf(0);
+let lastIndexArr10_1 = arr10_1.lastIndexOf(0);
+let indexArr10_2 = arr10_2.indexOf(0);
+let lastIndexArr10_2 = arr10_2.lastIndexOf(0);
+let sum1 = 0,
+    sum2 = 0;
+if (indexArr10_1 !== lastIndexArr10_1){
+    let buffer = arr10_1.slice(indexArr10_1,lastIndexArr10_1);
+    while (buffer.length){
+        sum1 += buffer.pop();
+    }
+}
+console.log(sum1);
+
+if (indexArr10_2 !== lastIndexArr10_2){
+    let buffer = arr10_1.slice(indexArr10_2,lastIndexArr10_2);
+    while (buffer.length){
+        sum2 += buffer.pop();
+    }
+}
+console.log(sum2);
+//task11
+let h = +prompt('Введите высоту треугольника');
+let a = [];
+for(let i=0; i < h; i++){
+    let str = '';
+    for(let j = 0; j < h-i; j++){
+        str += ' '
+    }
+    let p = ' ';
+    for( let k = 0; k< i*2-1; k++){
+        p+= '^';
+    }
+    a.push(str + p+ str);
+}
+for(let elem of a){
+    console.log(elem)
+}
